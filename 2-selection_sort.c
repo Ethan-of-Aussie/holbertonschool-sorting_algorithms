39542feedf78c452fce6b6e4ea17a22ff0d64fd9
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-04-12 03:07:52 +0000

    T.2 check

---
+++ 2-selection_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+/**
+ *swap - Function to swap two elements
+ *@x: First element
+ *@y: Second element
+ */
+void swap(int* x, int* y)
+{
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
+/**
+ *selection_sort - Sorting array with Selection sort algo
+ *
+ *@array: The Array of elements
+ *@size: Stored Size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+	size_t min;
+
+	for (i = 0; i < size - 1; i++)
+		{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+		if (array[j] < array[min])
+		min = j;
+		}
+	if (min != i)
+	swap(&array[i], &array[min]);
+	print_array(array, size);
+		}
+}
